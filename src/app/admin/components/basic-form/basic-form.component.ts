147dfa51d95fc8e999730cca1721b8eb0807ae77
joseSincal <ottozincal@gmail.com>
2024-09-20 17:25:55 -0600

    inputs

---
+++ src/app/admin/components/basic-form/basic-form.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from "@angular/core";
+import { FormControl } from "@angular/forms";
+
+@Component({
+    selector: "app-basic-form",
+    templateUrl: "./basic-form.component.html",
+    styleUrls: ["./basic-form.component.scss"],
+})
+export class BasicFormComponent implements OnInit {
+    nameField = new FormControl("");
+    emailField = new FormControl("");
+    phoneField = new FormControl("");
+    colorField = new FormControl("#000000");
+    dateField = new FormControl("");
+    ageField = new FormControl(12);
+
+    categoryField = new FormControl("category-1");
+    tagField = new FormControl("");
+
+    agreeField = new FormControl(false);
+    genderField = new FormControl("");
+    zoneField = new FormControl("");
+
+    constructor() {}
+
+    ngOnInit(): void {
+        this.nameField.valueChanges.subscribe((value) => {
+            console.log(value);
+        });
+    }
+
+    getNameValue() {
+        console.log(this.nameField.value);
+    }
+}
